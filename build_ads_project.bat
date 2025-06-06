641e4bb396927ccae14c231ee921a11ce28e6cbe
sweta <swetabardemirafra.com>
2025-06-06 12:27:25 +0530

    Signed-off-by: sweta <swetabardemirafra.com>

--- build_ads_project.bat
+++ build_ads_project.bat
@@ -1,11 +1,1 @@
-@echo off
-set ADS_PATH=C:\Infineon\AURIX-Studio-1.10.10
-set WORKSPACE=C:\Users\admin\AURIX-v1.10.10-workspace
-set PROJECT=code_test
-
-"%ADS_PATH%\launch-tool\launch-tool.exe" ^
--vmargs "%ADS_PATH%\plugins\org.eclipse.justj.openjdk.hotspot.jre.full.win32.x86_64_17.0.11.v20240426-1830\jre\bin\server\jvm.dll" ^
--vmargs-nosplash ^
--vmargs-application org.eclipse.cdt.managedbuilder.core.headlessbuild ^
--data "%WORKSPACE%" ^
--build %PROJECT%/Debug
+C:\Infineon\AURIX-Studio-1.10.10\tools\make\sh.exe -c "cd C:/Users/admin/Documents/demo_project && make all"
